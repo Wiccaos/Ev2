d551d7fe1f57910868468a771677390779c4f7b7
Leonardo Gonzalez <gonzalezleonardo042@gmail.com>
2024-10-22 16:42:31 -0300

    Conexion a BD Leo

---
+++ Empresa/DAL/db.py
@@ -0,0 +1,29 @@
+import mysql.connector
+from mysql.connector 
+
+def generar_conexion(user,password,server,database, consulta):
+    config={
+        'user': user,
+        'password': password,
+        'host': server,
+        'database': database
+    }
+    try:
+        conexion = mysql.connector.connect(**config)
+        if conexion and conexion.is_connected():
+            cursor = conexion.cursor()
+            cursor.execute(consulta)
+            if cursor != None:
+                for registro in cursor:
+                    print(registro)
+                    
+                    # for (first_name, last_name, hire_date) in cursor:
+                    #     print("{}, {} was hired on {:%d %b %Y}".format(
+                    #         last_name, first_name, hire_date))
+
+                cursor.close()
+            else:
+                print("Su búsqueda no arrojó resultados...")
+            conexion.close()
+        else:
+            print("Could not connect")
